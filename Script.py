8a54f97e533fa94584979fc8eedc7ed293f18e61
g24ai1002 <g24ai1002@iitj.ac.in>
2025-03-02 22:30:02 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,1 +1,7 @@
+from datetime import datetime
+
+now = datetime.now()
+formatted_date = now.strftime("%Y-%m-%d %H:%M:%S")
 print("Hello from Google Cloud VM!")
+print("Name: Shubham Verma - Roll No: G24AI1002")
+print("Current date and time:", formatted_date)
